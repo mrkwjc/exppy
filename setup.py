3516f270e470cc4ee5e5063ef675de1822013574
Marek Wojciechowski <mrkwjc@users.noreply.github.com>
2021-04-03 00:09:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="exppy",
-    version="0.9.2",
+    version="0.1.0",
     author="Marek Wojciechowski",
     author_email="mrkwjc@gmail.com",
     description="Numerical experiments in python",
